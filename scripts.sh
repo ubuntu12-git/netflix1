1452ed6527e5d8d269631077a75c66016076772b
ubuntu12-git <saraswatimadhyasch@gmail.com>
2024-02-10 21:28:24 +0530

    netflix1

---
+++ scripts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "data is availble here" > f1.txt
+echo "data is availble here" > f2.txt
+echo "data is availble here" > f3.txt
+echo "data is availble here" > f4.txt
+echo "data is availble here" > f5.txt
+echo "data is availble here" > f6.txt
+echo "data is availble here" > f7.txt
+echo "data is availble here" > f8.txt
+echo "data is availble here" > f9.txt
+echo "data is availble here" > f10.txt
+# branch creation 
+git branch feature1
+git branch feature2
+git branch feature3
+git branch feature4
+git branch feature5
+git branch feature6
+git branch feature7
+git branch feature8
+git branch feature9
+git branch feature10
+
